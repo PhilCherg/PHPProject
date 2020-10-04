272daac13a0122235ad1e2786ff41465ea56288c
Phillip Cherganski <phil_cherg@yahoo.com>
2020-10-04 20:08:57 +0300

    Student Assignments View

--- root/app/controllers/student.php
+++ root/app/controllers/student.php
@@ -19,7 +19,7 @@
             try {
                 $student = $this->db->executeSQL("SELECT * FROM Students LEFT JOIN Classes ON class_id = Classes.id WHERE Students.id = :id;", ['id' => $_SESSION['id']]);
                 $subjects = $this->db->executeSQL("SELECT * FROM Classes LEFT JOIN Class_Teachers ON Classes.id = Class_Teachers.class_id LEFT JOIN Teachers ON Class_Teachers.teacher_id = Teachers.id LEFT JOIN Subjects ON Teachers.subject_id = Subjects.id WHERE Classes.id = :class_id;", ['class_id' => $student[0]['class_id']]);
-                $assignments = $this->db->executeSQL("SELECT * FROM Assignments WHERE student_id = :id", ['id' => $_SESSION['id']]);
+                $assignments = $this->db->executeSQL("SELECT * FROM Student_Assignments LEFT JOIN Assignments ON Student_Assignments.assignment_id = Assignments.id LEFT JOIN Students ON Student_Assignments.student_id = Students.id WHERE Students.id = :id;", ['id' => $_SESSION['id']]);
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
--- root/app/views/student/assignments.php
+++ root/app/views/student/assignments.php
@@ -8,9 +8,9 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
             $(document).ready(function(){
-                $('.subjectTable').slideToggle(200);
+                $('.toggleTable').slideToggle(0);
                 $('.title').click(function(){
-                    $(this).nextUntil('.title').slideToggle(200);
+                    $(this).nextUntil('.title').slideToggle(1);
                 });
             });
         </script>
@@ -22,23 +22,36 @@
                 <h1>Assignments Overview</h1>
                 <p>View all assignments in each of your subjects.</p>
             </div>
-            <?php
+            <?php 
                 foreach ($data['subjects'] as $subject) {
-                    echo "<h1 class='title'>".$subject['subject_name']."</h1>";
-                    echo "<table class='subjectTable'>
+                    echo "<h1 class='title'>".$subject['subject_name'];
+                    $average = 0;
+                    $total = 0;
+                    foreach ($data['assignments'] as $assignment) {
+                        if ($assignment['assignment_points'] != null) {
+                            $total += round((float)$assignment['assignment_weight'], 2);
+                        }
+                    }
+                    foreach ($data['assignments'] as $assignment) {
+                        $score = round((float)$assignment['assignment_points'] / (float)$assignment['assignment_max'] * 100, 2);
+                        if ($total != 0) {
+                            $average += round($score * $assignment['assignment_weight'] / $total, 2);
+                        }
+                    }
+                    if ($average == 0) $average = "--";
+                    echo ": ".$average."%</h1>
+                        <table class='toggleTable'>
                         <tr>
-                            <th class='smallCol'>Id</th>
-                            <th>Assignment Name</th>
-                            <th>Assignment Grade</th>
-                            <th>Assignment Weight</th>
+                            <th>Assignment</th>
+                            <th class='smallCol'>Score</th>
+                            <th class='smallCol'>Weight</th>
                         </tr>";
-                    foreach($data['assignments'] as $assignment) {
+                    foreach ($data['assignments'] as $assignment) {
                         if ($assignment['subject_id'] == $subject['id']) {
                             echo "<tr>
-                                    <td>".$assignment['id']."</td>
-                                    <td>".$assignment['assignment_title']."</td>
-                                    <td>".$assignment['assignment_grade']."</td>
-                                    <td>".$assignment['assignment_weight']."</td>
+                                <td>".$assignment['assignment_title']."</td>
+                                <td>".$assignment['assignment_points']." / ".$assignment['assignment_max']." (".round((float)$assignment['assignment_points'] / (float)$assignment['assignment_max'] * 100, 2)."%)</td>
+                                <td>".$assignment['assignment_weight']."%</td>
                             </tr>";
                         }
                     }
